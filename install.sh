e6dffa2fd0a78776a543b401b4da98ea59a5707f
Cliff Erson <cerson@me.com>
2013-04-21 17:53:25 -0700

    update install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SOLOISTDIR="${HOME}/soloistgo"
+SOLOISTRC_URL="https://raw.github.com/clifferson/soloist-config/master/soloistrc"
+CHEFFILE_URL="https://raw.github.com/clifferson/soloist-config/master/Cheffile"
+
+echo "Creating .soloistgo dir"
+mkdir ${SOLOISTDIR}
+
+echo "Downloading soloistrc"
+curl ${SOLOISTRC_URL} -o ${SOLOISTDIR}/soloistrc
+
+echo "Downloading Cheffile"
+curl ${CHEFFILE_URL} -o ${SOLOISTDIR}/Cheffile
+
+echo "Installing soloist gem"
+sudo gem install soloist --no-rdoc --no-ri
+
+echo "Running soloist"
+cd ${SOLOISTDIR} && soloist
+
+echo "Delete ${SOLOISTDIR}? [y/n]:"
+read DELETE_SOLOISTGO
+
+if [ $DELETE_SOLOISTGO == 'y' ]; then
+  echo "Deleting ${SOLOISTDIR}"
+  rm -rf ${SOLOISTDIR}
+fi
