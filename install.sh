b5ffea3f65973f325c753203f6402a94a5fb0fbb
Cliff Erson <cerson@me.com>
2013-06-20 21:14:46 -0700

    update install.sh to install omnibus chef

--- install.sh
+++ install.sh
@@ -4,8 +4,15 @@ SOLOISTDIR="${HOME}/soloistgo"
 SOLOISTRC_URL="https://raw.github.com/clifferson/soloist-config/master/soloistrc"
 CHEFFILE_URL="https://raw.github.com/clifferson/soloist-config/master/Cheffile"
 
+# We are going to install omnibus chef and then use its embedded ruby/gem for soloist.
+OMNIBUS_CHEFCLIENT_URL="https://www.opscode.com/chef/install.sh"
+CHEF_EMBEDDED_PATH="/opt/chef/embedded/bin"
+
 echo "I can has root?"
 sudo true
+
+echo "Installing omnibus chef."
+curl -L https://www.opscode.com/chef/install.sh | bash
 
 echo "Creating .soloistgo dir"
 mkdir ${SOLOISTDIR}
@@ -17,11 +24,11 @@ echo "Downloading Cheffile"
 curl ${CHEFFILE_URL} -o ${SOLOISTDIR}/Cheffile
 
 echo "Installing soloist & plist gem"
-if [ ! $(gem specification soloist &>/dev/null) ]; then sudo gem install soloist --no-rdoc --no-ri; fi
-sudo gem install plist --no-rdoc --no-ri
+if [ ! $(gem specification soloist &>/dev/null) ]; then sudo ${CHEF_EMBEDDED_PATH}/gem install soloist --no-rdoc --no-ri; fi
+if [ ! $(gem specification plist &>/dev/null) ]; then sudo ${CHEF_EMBEDDED_PATH}/gem install plist --no-rdoc --no-ri; fi
 
 echo "Running soloist"
-cd ${SOLOISTDIR} && sudo soloist
+cd ${SOLOISTDIR} && sudo ${CHEF_EMBEDDED_PATH}/soloist
 
 if [ $? -eq 0 ]; then
   echo "Soloist run complete, you might want to delete ${SOLOISTDIR}?"
